5a0edb634bc6eaab3aca86ede77253e14d552538
hnlcf <louchangfeng@outlook.com>
2021-12-10 20:45:07 +0800

    refactor(all): first refactory, make modular code
    1. rewrite CPU and MEMORY, R/W interface for MEMORY, functions related to print info, decode of instructions and operands. 
    2. correct make commands. 
    3. update .gitignore.

---
+++ src/common/print.c
@@ -0,0 +1,21 @@
+
+#include <assert.h>
+#include <headers/common.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+// wrapper of stdio  printf
+// controlled by the debug verbose bit set
+uint64_t debug_print(uint64_t open_set, const char *format, ...) {
+  if ((open_set & DEBUG_VERBOSE_SET) == 0x0) {
+    return 0x1;
+  }
+
+  // implement of stdio printf()
+  va_list argptr;
+  va_start(argptr, format);
+  vfprintf(stderr, format, argptr);
+  va_end(argptr);
+
+  return 0x0;
+}
--- src/cpu/mmu.c
+++
@@ -1,4 +0,0 @@
-#include "cpu/mmu.h"
-#include "memory/dram.h"
-
-uint64_t va2pa(uint64_t v_addr) { return v_addr % MM_LEN; }
