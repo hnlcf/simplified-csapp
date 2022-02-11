a024ea6ea136d5c32225bdfe524028e425dfeb49
Changfeng <louchangfeng@outlook.com>
2022-02-11 11:53:39 +0800

    feat(isa): assistant lookup table for parsing operand

--- src/hardware/cpu/isa.c
+++ src/hardware/cpu/isa.c
@@ -131,6 +131,73 @@ static void
 parse_instruction(const char* str, inst_t* inst, core_t* cr)
 {}
 
+// lookup table
+static const char* reg_name_list[72] = {
+  "%rax", "%eax",  "%ax",   "%ah",   "%al",  "%rbx",  "%ebx",  "%bx",
+  "%bh",  "%bl",   "%rcx",  "%ecx",  "%cx",  "%ch",   "%cl",   "%rdx",
+  "%edx", "%dx",   "%dh",   "%dl",   "%rsi", "%esi",  "%si",   "%sih",
+  "%sil", "%rdi",  "%edi",  "%di",   "%dih", "%dil",  "%rbp",  "%ebp",
+  "%bp",  "%bph",  "%bpl",  "%rsp",  "%esp", "%sp",   "%sph",  "%spl",
+  "%r8",  "%r8d",  "%r8w",  "%r8b",  "%r9",  "%r9d",  "%r9w",  "%r9b",
+  "%r10", "%r10d", "%r10w", "%r10b", "%r11", "%r11d", "%r11w", "%r11b",
+  "%r12", "%r12d", "%r12w", "%r12b", "%r13", "%r13d", "%r13w", "%r13b",
+  "%r14", "%r14d", "%r14w", "%r14b", "%r15", "%r15d", "%r15w", "%r15b",
+};
+
+static uint64_t
+reflect_register(const char* str, core_t* cr)
+{
+  // lookup table
+  reg_t* reg = &(cr->reg);
+  uint64_t reg_addr[72] = {
+    (uint64_t) & (reg->rax),  (uint64_t) & (reg->eax),
+    (uint64_t) & (reg->ax),   (uint64_t) & (reg->ah),
+    (uint64_t) & (reg->al),   (uint64_t) & (reg->rbx),
+    (uint64_t) & (reg->ebx),  (uint64_t) & (reg->bx),
+    (uint64_t) & (reg->bh),   (uint64_t) & (reg->bl),
+    (uint64_t) & (reg->rcx),  (uint64_t) & (reg->ecx),
+    (uint64_t) & (reg->cx),   (uint64_t) & (reg->ch),
+    (uint64_t) & (reg->cl),   (uint64_t) & (reg->rdx),
+    (uint64_t) & (reg->edx),  (uint64_t) & (reg->dx),
+    (uint64_t) & (reg->dh),   (uint64_t) & (reg->dl),
+    (uint64_t) & (reg->rsi),  (uint64_t) & (reg->esi),
+    (uint64_t) & (reg->si),   (uint64_t) & (reg->sih),
+    (uint64_t) & (reg->sil),  (uint64_t) & (reg->rdi),
+    (uint64_t) & (reg->edi),  (uint64_t) & (reg->di),
+    (uint64_t) & (reg->dih),  (uint64_t) & (reg->dil),
+    (uint64_t) & (reg->rbp),  (uint64_t) & (reg->ebp),
+    (uint64_t) & (reg->bp),   (uint64_t) & (reg->bph),
+    (uint64_t) & (reg->bpl),  (uint64_t) & (reg->rsp),
+    (uint64_t) & (reg->esp),  (uint64_t) & (reg->sp),
+    (uint64_t) & (reg->sph),  (uint64_t) & (reg->spl),
+    (uint64_t) & (reg->r8),   (uint64_t) & (reg->r8d),
+    (uint64_t) & (reg->r8w),  (uint64_t) & (reg->r8b),
+    (uint64_t) & (reg->r9),   (uint64_t) & (reg->r9d),
+    (uint64_t) & (reg->r9w),  (uint64_t) & (reg->r9b),
+    (uint64_t) & (reg->r10),  (uint64_t) & (reg->r10d),
+    (uint64_t) & (reg->r10w), (uint64_t) & (reg->r10b),
+    (uint64_t) & (reg->r11),  (uint64_t) & (reg->r11d),
+    (uint64_t) & (reg->r11w), (uint64_t) & (reg->r11b),
+    (uint64_t) & (reg->r12),  (uint64_t) & (reg->r12d),
+    (uint64_t) & (reg->r12w), (uint64_t) & (reg->r12b),
+    (uint64_t) & (reg->r13),  (uint64_t) & (reg->r13d),
+    (uint64_t) & (reg->r13w), (uint64_t) & (reg->r13b),
+    (uint64_t) & (reg->r14),  (uint64_t) & (reg->r14d),
+    (uint64_t) & (reg->r14w), (uint64_t) & (reg->r14b),
+    (uint64_t) & (reg->r15),  (uint64_t) & (reg->r15d),
+    (uint64_t) & (reg->r15w), (uint64_t) & (reg->r15b),
+  };
+
+  for (int i = 0; i < 72; ++i) {
+    if (strcmp(str, reg_name_list[i]) == 0) {
+      // get i is
+      return reg_addr[i];
+    }
+  }
+  printf("parse register %s error\n", str);
+  exit(0);
+}
+
 static void
 parse_operand(const char* str, od_t* od, core_t* cr)
 {}
