a41daf9782ed4caa897db99681fa8a05b6754d01
Changfeng <89689286+hnlcf@users.noreply.github.com>
2022-06-19 00:39:08 +0800

    build: modify cmake project config and python build script (#7)
    * build(cmake): set debug compile flag and add test
    
    1. Add project detail information
    2. Reset debug compile flag
    3. Change source files include ways for target
    4. Add custom test
    
    * build(script): add test option
    
    1. Add cmake test command option
    2. Modify cmake config and build command

--- tools.py
+++ tools.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-
 import os
 import sys
 
@@ -8,21 +7,25 @@ project_path = os.getcwd()
 build_path = project_path + '/build'
 
 cmake_p = 'cmake'
-build_mode = 'Debug'
+build_mode = 'debug'
 generator_opt = '-G "Unix Makefiles"'
 
 cmake_config_cmd = f'{cmake_p} {generator_opt} -S {project_path} -DCMAKE_BUILD_TYPE={build_mode} -B {build_path}'
 cmake_build_cmd = f'{cmake_p} --build {build_path} --config {build_mode} --parallel 12 --target all'
-cmake_clean_cmd = f'{cmake_p} --build {build_path} --target clean -- -j 12'
+cmake_clean_cmd = f'{cmake_p} --build {build_path} --target clean'
+cmake_test_cmd = f'ctest -C {build_mode} --verbose'
 
 
 def main():
     if sys.argv[1] == '--config':
-        print(cmake_config_cmd)
+        os.system(cmake_config_cmd)
     elif sys.argv[1] == '--build':
-        print(cmake_build_cmd)
+        os.system(cmake_build_cmd)
     elif sys.argv[1] == '--clean':
-        print(cmake_clean_cmd)
+        os.system(cmake_clean_cmd)
+    elif sys.argv[1] == '--test':
+        os.system(cmake_build_cmd)
+        os.system(f'cd {build_path} && {cmake_test_cmd}')
 
 
 if __name__ == '__main__':
